8affc52c6c81dffd42158f653f98813b7cc52057
Jon Jagger <jon@jaggersoft.com>
2020-07-04 14:30:05 +0100

    Add comment

--- sh/versioner_env_vars.sh
+++ sh/versioner_env_vars.sh
@@ -3,7 +3,7 @@
 versioner_env_vars()
 {
   docker run --rm cyberdojo/versioner
-  echo CYBER_DOJO_PULLER_CLIENT_PORT=4597
+  echo CYBER_DOJO_PULLER_CLIENT_PORT=4597 # runner
   echo CYBER_DOJO_PULLER_CLIENT_USER=nobody
   echo CYBER_DOJO_PULLER_SERVER_USER=nobody
 }
